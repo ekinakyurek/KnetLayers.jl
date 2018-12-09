e7fc4c956b344b3b9e4b5cc488b2cb8e8cb238f4
ekinakyurek <akyurekekin@gmail.com>
2018-12-08 20:59:40 -0500

    update test/header.jl

--- test/header.jl
+++ test/header.jl
@@ -1,3 +1,3 @@
-using Test,KnetLayers
+using Test,KnetLayers, Knet
 gpu() < 0 ?  KnetLayers.settype!(Array{Float64}) : KnetLayers.settype!(KnetArray{Float64})
 println("Testing with $(KnetLayers.arrtype)")
